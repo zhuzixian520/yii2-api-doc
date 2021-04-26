6feb83af19aae8df729d2f76ce6eb2f18e7c3422
zhujun <zhuzixian520@126.com>
2021-04-26 13:37:46 +0800

    首页增加注释助手函数

---
+++ src/AnnotationHelper.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace zhuzixian520\api_doc;
+
+class AnnotationHelper
+{
+    /**
+     * 通过控制器和其下action，获取action中的注释信息
+     * @param $className
+     * @param $methodName
+     * @return array
+     * @throws ReflectionException
+     */
+    public static function getNoteDetail($className, $methodName){
+        $notes = ['name'=>'', 'param'=>[], 'res'=>[], 'method'=>'', 'desc'=>'', 'exception'=>[], 'token'=> 0];
+        $description = '';
+        $returns = [];
+        $params = [];
+        $rMethod = new ReflectionMethod($className, $methodName);
+        $docComment = $rMethod->getDocComment();
+        $docCommentArr = explode("\n", $docComment);
+        foreach ($docCommentArr as $comment) {
+
+            $comment = trim($comment);
+            //标题描述
+            if (empty($description) && strpos($comment, '@') === false && strpos($comment, '/') === false) {
+                $description = substr($comment, strpos($comment, '*') + 1);
+                $notes['name'] = $description;
+                continue;
+            }
+
+            //@desc注释
+            $pos = stripos($comment, '@desc');
+            if ($pos !== false) {
+                $descComment = substr($comment, $pos + 5);
+                $notes['desc'] = $descComment;
+                continue;
+            }
+
+            //@exception注释
+            $pos = stripos($comment, '@exception');
+            if ($pos !== false) {
+                $exceptions[] = explode(' ', trim(substr($comment, $pos + 10)));
+                $notes['exception'] = $exceptions;
+                continue;
+            }
+
+            //@method注释
+            $pos = stripos($comment, '@method');
+            if ($pos !== false) {
+                $method = substr($comment, $pos + 8);
+                $notes['method'] = $method;
+                continue;
+            }
+
+            //@token注释
+            $pos = stripos($comment, '@token');
+            if ($pos !== false) {
+                $token = substr($comment, $pos + 7);
+                $notes['token'] = $token;
+                continue;
+            }
+
+            //@param注释
+            $pos = stripos($comment, '@param');
+            if ($pos !== false) {
+                $paramCommentArr = explode(' ', substr($comment, $pos + 7));
+                //$paramCommentArr = array_values(array_filter($paramCommentArr));
+                $params[] = $paramCommentArr;
+                $notes['param'] = $params;
+                continue;
+            }
+
+            //@res注释
+            $pos = stripos($comment, '@res');
+            if ($pos === false) {
+                continue;
+            }
+            $returnCommentArr = explode(' ', substr($comment, $pos + 5));
+            //将数组中的空值过滤掉，同时将需要展示的值返回
+            $returnCommentArr = array_values(array_filter($returnCommentArr));
+            if (count($returnCommentArr) < 2) {
+                continue;
+            }
+            if (!isset($returnCommentArr[2])) {
+                $returnCommentArr[2] = '';	//可选的字段说明
+            } else {
+                //兼容处理有空格的注释
+                $returnCommentArr[2] = implode(' ', array_slice($returnCommentArr, 2));
+
+            }
+            $returns[] = $returnCommentArr;
+            $notes['res'] = $returns;
+        }
+
+        return $notes;
+    }
+}
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -3,8 +3,12 @@
 namespace zhuzixian520\api_doc\controllers;
 
 use Yii;
+use yii\helpers\FileHelper;
+use yii\helpers\Inflector;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 use yii\web\Response;
+use zhuzixian520\api_doc\AnnotationHelper;
 
 class DefaultController extends Controller
 {
@@ -26,6 +30,67 @@ class DefaultController extends Controller
     {
         $this->layout = 'main';
 
+        $version = (int)Yii::$app->request->get('v', 1);
+        if ($version == 0){
+            $version = 'v1';
+        }else {
+            $version = 'v' . $version;
+        }
+
+        $apiModuleList = $this->_getApiModuleList($version);
+
+        $data = [
+            'apiModuleList' => $apiModuleList,
+            'ver' => $version,
+        ];
+
         return $this->render('index');
+    }
+
+    private function _getApiModuleList($version)
+    {
+        //$yii2_modules = Yii::$app->modules;
+        //unset($yii2_modules['gii']);
+        //unset($yii2_modules['debug']);
+        $module = Yii::$app->getModule($version);//获取版本模块
+        if (!$module) {
+            throw new NotFoundHttpException('您输入的地址页面不存在');
+        }
+
+        $controller_path = $module->getControllerPath();//获取控制器主路径
+
+        //$folder_arr = FileHelper::findDirectories($controller_path);//获取控制器主文件夹下的子文件数组
+        $folder = $controller_path;
+        $apiModuleList = [];
+        //foreach ($folder_arr as $k => $folder) {
+        $controllers = FileHelper::findFiles($folder, ['recursive' => true]);
+        foreach ($controllers as $k2 => $controller) {
+            //获取控制器的类名，含命名空间
+            $base_path = dirname(Yii::getAlias('@api_cms'));
+            $c_name_temp = str_replace([$base_path, '.php', '/'], ['', '', '\\'], $controller);
+            $c_name = mb_substr($c_name_temp, 1);
+
+            $contents = file_get_contents($controller);
+            $controllerId = Inflector::camel2id(substr(basename($controller), 0, -14));
+
+            preg_match_all('/public function action(\w+?)\(/', $contents, $result);
+            foreach ($result[1] as $k3 => $action) {
+                $methods = AnnotationHelper::getNoteDetail($c_name, 'action' . $action);
+                $actionId = Inflector::camel2id($action);
+
+                /*$ctrl = str_replace($controller_path . DIRECTORY_SEPARATOR, '', $folder_arr[$k]);
+                $route = $ctrl . '/' . $controllerId . '/' . $actionId;
+                $apiModuleList[$ctrl][$route] = $methods;
+                $apiModuleList[$ctrl][$route]['route'] = $route;
+                asort($apiModuleList[$ctrl][$route]);*/
+
+                $route = $controllerId . '/' . $actionId;
+                $apiModuleList[$route] = $methods;
+                $apiModuleList[$route]['route'] = $route;
+                asort($apiModuleList[$route]);
+            }
+        }
+        //}
+        return $apiModuleList;
     }
 }
